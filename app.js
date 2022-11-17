3a8788c442a36513862f293959fddad954b723d3
3ddw1nn <edd@edd-lee.com>
2022-11-17 12:04:08 +0700

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB =
-  "mongodb+srv://eddw1nn:2bananas@cluster0.iralrgs.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url =
+"mongodb+srv://eddw1nn:2bananas@cluster0.iralrgs.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
